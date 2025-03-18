21b0c86c01994b969990c1df91445e726e1393e0
Blessing Akinola <akinolablessing25@gmail.com>
2025-03-18 15:38:22 +0100

    initial project set-up completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,31 @@
 // import { useState } from 'react'
 
-import './App.css'
-import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import AboutUs from './pages/Aboutus';
+import ContactUs from './pages/Contactus';
+import Events from './pages/Events';
+import Contents from './pages/Contents';
+import SingleEvent from './pages/SingleEvent';
 
 function App() {
 
   return (
     <>
       < Navbar />
+      <Routes>
+        <Route path='/' element={< Home />} />
+        <Route path='/events' element={< Events />} />
+        <Route path='/events/:id' element={< SingleEvent />} />
+        <Route path='/contents' element={< Contents />} />
+        <Route path='/about-us' element={< AboutUs />} />
+        <Route path='/contact-us' element={< ContactUs />} />
+      </Routes> 
     </>
   )
 }
 
 export default App
+ 
