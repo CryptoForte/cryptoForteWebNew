3b071bbc5e7c19420b366f46291f3b76948243dc
Blessing Akinola <akinolablessing25@gmail.com>
2025-03-18 14:51:17 +0100

    page set-up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,20 @@
 // import { useState } from 'react'
 
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
+import Home from './pages/Home'
 
 function App() {
 
   return (
-    <div>
+    <>
       < Navbar />
-    </div>
+
+      <Routes>
+        <Route path='/' element= {< Home />} />
+      </Routes>
+    </>
   )
 }
 
