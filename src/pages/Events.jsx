10ef505767bfb7a62a9a05934e89e16ab4428b1e
Philip-D21 <philiplekan88@gmail.com>
2025-03-20 11:15:21 +0100

    feat: implementing the header and the footer section

---
+++ src/pages/Events.jsx
@@ -0,0 +1,11 @@
+
+const Events = () => {
+  return (
+    <div>
+      <h1 className="text-4xl text-red-400 text-center mt-12">Events</h1> 
+      
+    </div>
+  )
+}
+
+export default Events
---
+++ src/pages/SingleEvent.jsx
@@ -0,0 +1,10 @@
+
+const SingleEvent = () => {
+  return (
+    <div>
+      <h1 className="text-4xl text-red-400 text-center mt-12">Contact Us</h1> 
+    </div>
+  )
+}
+
+export default SingleEvent
