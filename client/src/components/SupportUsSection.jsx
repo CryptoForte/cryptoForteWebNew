551748b58e5d27c499075eee038643b3145a5ea1
Blessing Akinola <akinolablessing25@gmail.com>
2025-03-21 13:35:56 +0100

    Home Page development progress

--- client/src/components/SupportUsSection.jsx
+++ client/src/components/SupportUsSection.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { MdKeyboardArrowRight } from 'react-icons/md';
 import { IoCloseCircleOutline } from 'react-icons/io5';
 import { TbCopyCheckFilled } from 'react-icons/tb';
@@ -12,14 +12,22 @@ const DonationModal = () => {
     setCopy(true);
     navigator.clipboard.writeText('0x3725340694BaF2F3e116d47e1460A8d394E7BD4b');
 
-    // Reset the copy state back to false after 30 seconds
     setTimeout(() => {
       setCopy(false);
-    }, 3000); // 30 seconds in milliseconds
+    }, 3000); 
   };
 
   return (
-    <>
+    <section className='max-w-6xl mx-auto '>
+      <div className='text-center'>
+        <h1 className='text-xl md:text-4xl font-bold my-4'>Support the Future of Web3</h1>
+        <p className='px-4 md:px-32'>
+          Your donation helps us to make crypto simple, accessible and beginner 
+          friendly. Every contrubution counts, ensuring secure and transparent giving 
+          while empowering more people to navigate Web3 with confidence. 
+          Be part of the change - donate now and shape the future of crypto
+        </p>
+      </div>
       {/* Donate Button */}
       <div className="flex justify-center items-center my-4">
         <article
@@ -48,8 +56,8 @@ const DonationModal = () => {
               className="text-2xl font-semibold absolute top-2 right-2 cursor-pointer"
               onClick={() => setIsModalOpen(false)}
             />
-            <h1 className="text-xl py-4 font-bold text-slate-700">Support our Case</h1>
-            <p>Kindly send your donation to the wallet address below</p>
+            <h1 className="text-xl py-4 font-bold text-slate-700">Support our Course</h1>
+            <p className='text-base fontsemibold'>Kindly send your donation to the wallet address below</p>
             <div className="py-3">
               <h5>
                 We accepts payments in <span className="block my-2">USDT / ERC20 / BEP20</span>
@@ -68,7 +76,7 @@ const DonationModal = () => {
           </div>
         </div>
       )}
-    </>
+    </section>
   );
 };
 
