deec184fbbc413d85b782ab57cb02e6d89ca4354
Blessing Akinola <akinolablessing25@gmail.com>
2025-04-04 14:11:12 +0100

    Events Page progress

---
+++ client/src/datas/events.js
@@ -0,0 +1,64 @@
+
+import BuildCon from '../assets/buildcon001.png';
+import BuildCon1 from '../assets/buildcon002.png';
+import BuildCon2 from '../assets/buildcon003.png';
+import BuildCon3 from '../assets/buildcon004.png';
+import BuildCon4 from '../assets/buildcon005.png';
+import BuildCon5 from '../assets/buildcon006.png';
+
+export const eventList = [
+    {
+      id: 1,
+      name: "Keni Golf",
+      address: "Lagos, Nigeria",
+      date: "12th April, 2025",
+      link: '/seemore',
+      img: BuildCon,
+      event_state: "LIVE"
+    },
+    {
+      id: 2,
+      name: "Dame Rafer",
+      address: "Ife, Nigeria",
+      date: "31th June, 2025",
+      link: '/seemore',
+      img: BuildCon1,
+      event_state: "UPCOMING"
+    },
+    {
+      id: 3,
+      name: "Keni Golf",
+      address: "Lagos, Nigeria",
+      date: "12th April, 2025",
+      link: '/seemore',
+      img: BuildCon2,
+      event_state: "COMPLETED"
+    },
+    {
+      id: 4,
+      name: "Keni Golf",
+      address: "Lagos, Nigeria",
+      date: "12th April, 2025",
+      link: '/seemore',
+      img: BuildCon3,
+      event_state: "UPCOMING"
+    },
+    {
+      id: 5,
+      name: "Keni Golf",
+      address: "Lagos, Nigeria",
+      date: "12th April, 2025",
+      link: '/seemore',
+      img: BuildCon4,
+      event_state: "LIVE"
+    },
+    {
+      id: 6,
+      name: "Keni Golf",
+      address: "Lagos, Nigeria",
+      date: "12th April, 2025",
+      link: '/seemore',
+      img: BuildCon5,
+      event_state: "UPCOMING"
+    },
+  ];
